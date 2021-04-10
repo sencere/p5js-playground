71ffc36fd556fc1e63cbce7ec03910b60205b334
sencere <morpheusspirit003@gmail.com>
2021-04-10 22:44:17 +0200

    fix codestyle, add first steps of the firefly algorithm

---
+++ firefly_algorithm/sketch.js
@@ -0,0 +1,49 @@
+let fireflyArray = [];
+let fr = 30;
+let interval = 5000;
+
+var myVar = setInterval(myTimer, interval);
+
+function myTimer() {
+  this.fireflyArray = [];
+  this.createFireflies(155);
+}
+
+function myStopFunction() {
+  clearInterval(myVar);
+}
+
+function setup() {
+  createCanvas(400, 400);
+  this.createFireflies(155);
+}
+
+function draw() {
+  background(220);
+  frameRate(fr);
+
+  for (let i = 0; i < fireflyArray.length; i++) {
+    fireflyArray[i].display();
+  }
+}
+
+function Firefly(m, x, y) {
+  this.mass = m;
+  this.position = createVector(x, y);
+  this.acceleration = createVector(0, 0);
+  this.velocity = createVector(0, 0);
+}
+
+function createFireflies(amount = 10) {
+  for (let i = 0; i < amount; i++) {
+    fireflyArray[i] = new Firefly(0.5, random(0, 400), random(0, 400));
+    fireflyArray[i].display();
+  }
+}
+
+Firefly.prototype.display = function() {
+  stroke(0);
+  strokeWeight(2);
+  fill(155, 200, 300);
+  ellipse(this.position.x, this.position.y, this.mass * 20, this.mass * 20);
+};
